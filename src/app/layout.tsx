a4487d42e011a485e8999141f184ba30d2c2957d
William Makino <wmakino@live.com>
2026-04-29 00:00:17 -0600

    feat: umami analytics implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-[var(--background)] text-[var(--foreground)]">
         {children}
       </body>
+      <Script src="https://analytics.wmakino.com/script.js" data-website-id="e927e61c-2364-4248-bf5e-65f46eac6318" />
     </html>
   );
 }
